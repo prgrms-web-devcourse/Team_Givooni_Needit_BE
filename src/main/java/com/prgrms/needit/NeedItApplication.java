5de6d6af86abef894495ccc74e84b2db4c6a1c01
김부희 <kimbuhui@naver.com>
2021-12-01 03:18:26 +0900

    initial setting

---
+++ src/main/java/com/prgrms/needit/NeedItApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.needit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prgrms/needit/NeedItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.needit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
