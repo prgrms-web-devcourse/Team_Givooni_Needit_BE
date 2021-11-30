99ebed3c27bccae96b341a9a90a03cac45edc699
김부희 <kimbuhui@naver.com>
2021-12-01 03:58:44 +0900

    Swagger 설정

---
+++ src/main/java/com/prgrms/needit/common/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.prgrms.needit.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private static final String[] AUTH_WHITELIST = {
+		"/swagger-resources/**",
+		"/swagger-ui.html",
+		"/v2/api-docs",
+		"/webjars/**"
+	};
+
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring()
+		   .antMatchers(AUTH_WHITELIST);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+			.disable();
+	}
+}
