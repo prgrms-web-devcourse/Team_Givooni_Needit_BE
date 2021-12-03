5eed96a0f35847aa9de479c12a8c3222d592a1ca
김부희 <kimbuhui@naver.com>
2021-12-03 15:01:03 +0900

    배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,6 +33,9 @@ echo "> $JAR_NAME 에 실행권한 추가" >> /home/ec2-user/app/deploy.log
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행" >> /home/ec2-user/app/deploy.log
+
+source /etc/profile
+
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
