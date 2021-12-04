ca7f5089d43b1dd7e5d63ef3c78a31a20843c425
Kwonkyu <park2348190@naver.com>
2021-12-05 02:18:20 +0900

    Updated common enum.

--- src/main/java/com/prgrms/needit/common/domain/enums/DonationCategory.java
+++ src/main/java/com/prgrms/needit/common/domain/enums/DonationCategory.java
@@ -2,5 +2,5 @@ package com.prgrms.needit.common.domain.enums;
 
 public enum DonationCategory {
 	GOODS,
-	ACTIVITY
+	TALENT
 }
--- src/main/java/com/prgrms/needit/common/domain/enums/DonationStatus.java
+++ src/main/java/com/prgrms/needit/common/domain/enums/DonationStatus.java
@@ -1,8 +1,6 @@
 package com.prgrms.needit.common.domain.enums;
 
 public enum DonationStatus {
-	REQUESTED,
-	ACCEPTED,
-	REFUSED,
-	CANCELLED
+	DONATING,
+	DONE
 }
