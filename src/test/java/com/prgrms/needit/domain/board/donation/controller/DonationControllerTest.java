462ce6921d95a6716d2b065fe7c3c7cf2e61765d
김부희 <kimbuhui@naver.com>
2021-12-05 17:28:02 +0900

    test : 기부글 수정, 거래상태 테스트

--- src/test/java/com/prgrms/needit/domain/board/donation/controller/DonationControllerTest.java
+++ src/test/java/com/prgrms/needit/domain/board/donation/controller/DonationControllerTest.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.prgrms.needit.common.BaseIntegrationTest;
 import com.prgrms.needit.domain.board.donation.dto.DonationRequest;
+import com.prgrms.needit.domain.board.donation.dto.DonationStatusRequest;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -21,6 +22,7 @@ class DonationControllerTest extends BaseIntegrationTest {
 	private static final String CATEGORY = "물품";
 	private static final String QUALITY = "상";
 	private static final List<Long> TAGS = new ArrayList<>(List.of(1L, 2L, 3L));
+	private static final String UPDATE_STATUS = "기부 완료";
 
 	@DisplayName("회원의 기부글 등록")
 	@Test
@@ -39,5 +41,41 @@ class DonationControllerTest extends BaseIntegrationTest {
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.message").value("success"))
 			.andExpect(jsonPath("$.data").exists());
+	}
+
+	@DisplayName("회원의 기부글 정보수정")
+	@Test
+	void modifyDonation() throws Exception {
+		// given
+		DonationRequest modifyRequest = new DonationRequest(
+			TITLE, CONTENT, CATEGORY, QUALITY, TAGS
+		);
+
+		// when then
+		this.mockMvc
+			.perform(MockMvcRequestBuilders
+						 .put("/donation/{id}", ID)
+						 .content(objectMapper.writeValueAsString(modifyRequest))
+						 .contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.message").value("success"))
+			.andExpect(jsonPath("$.data").value(ID));
+	}
+
+	@DisplayName("회원의 기부글 거래상태 변경")
+	@Test
+	void modifyDealStatus() throws Exception {
+		// given
+		DonationStatusRequest modifyStatusRequest = new DonationStatusRequest(UPDATE_STATUS);
+
+		// when then
+		this.mockMvc
+			.perform(MockMvcRequestBuilders
+						 .patch("/donation/{id}", ID)
+						 .content(objectMapper.writeValueAsString(modifyStatusRequest))
+						 .contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.message").value("success"))
+			.andExpect(jsonPath("$.data").value(ID));
 	}
 }
