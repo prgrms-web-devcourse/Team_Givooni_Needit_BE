afa830a9d0ef6a77a2ac3c12d4279e049ca80e9a
Kwonkyu <park2348190@naver.com>
2021-12-06 21:01:50 +0900

    Update src/main/java/com/prgrms/needit/common/error/ErrorCode.java
    Co-authored-by: buri-1029 <53345195+buri-1029@users.noreply.github.com>

--- src/main/java/com/prgrms/needit/common/error/ErrorCode.java
+++ src/main/java/com/prgrms/needit/common/error/ErrorCode.java
@@ -4,9 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-	INTERNAL_SERVER_ERROR(500, "C001", "정의되지 않은 에러가 발생했습니다."),
-	CONTRACT_NOT_FOUND(404, "???", "해당 기부 예약을 찾을 수 없습니다."), // message source 적용?
-	INVALID_INPUT_VALUE(400, "C002", "올바른 입력 형식이 아닙니다.");
+        INTERNAL_SERVER_ERROR(500, "C001", "정의되지 않은 에러가 발생했습니다."),
+        INVALID_INPUT_VALUE(400, "C002", "올바른 입력 형식이 아닙니다."),
+        
+	NOT_FOUND_CONTRACT(404, "???", "해당 기부 예약을 찾을 수 없습니다."); // message source 적용?
 
 	private final String code;
 	private final String message;
