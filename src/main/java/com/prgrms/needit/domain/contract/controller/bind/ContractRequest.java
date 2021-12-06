4255f97c30e6a9582428667cc10ac022f0724c30
Kwonkyu <park2348190@naver.com>
2021-12-06 21:59:18 +0900

    [style] : applied code review #2 - 불필요한 setter 제거.

--- src/main/java/com/prgrms/needit/domain/contract/controller/bind/ContractRequest.java
+++ src/main/java/com/prgrms/needit/domain/contract/controller/bind/ContractRequest.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class ContractRequest {
 
 	@JsonProperty("contractDate")
--- src/main/java/com/prgrms/needit/domain/message/controller/bind/ChatMessageRequest.java
+++ src/main/java/com/prgrms/needit/domain/message/controller/bind/ChatMessageRequest.java
@@ -3,18 +3,14 @@ package com.prgrms.needit.domain.message.controller.bind;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class ChatMessageRequest {
 
-	// 채팅 텍스트.
 	@JsonProperty("content")
 	@NotBlank
 	private String content;
 
-	// 채팅에 포함할 계약 식별자. 즉 계약은 사전에 생성 후 채팅 생성 요청에 포함하여 전송.
 	@JsonProperty("contractId")
 	private Long contractId;
 
