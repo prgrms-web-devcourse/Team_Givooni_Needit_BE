27244549862fbc82c943abdf0310c82c942a7054
이하정 <961210eee@naver.com>
2021-12-08 17:28:49 +0900

    style : 시큐리티 관련 추가된 부분 및 주석 삭제

--- src/main/java/com/prgrms/needit/common/config/SecurityConfig.java
+++ src/main/java/com/prgrms/needit/common/config/SecurityConfig.java
@@ -1,17 +1,13 @@
 package com.prgrms.needit.common.config;
 
-import com.prgrms.needit.domain.member.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +19,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		"/v2/api-docs",
 		"/webjars/**"
 	};
-	@Autowired
-	private MemberService memberService;
 
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
@@ -43,37 +37,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf()
 			.disable();
-
-		http.authorizeRequests()
-			.antMatchers("/member/**")
-			.authenticated()
-			.antMatchers("/admin/**")
-			.authenticated()
-			.antMatchers("/**")
-			.permitAll();
-
-		// 로그인 설정
-		http.formLogin()
-			.loginPage("/login")
-			.defaultSuccessUrl("/")
-			.permitAll();
-
-		// 로그아웃 설정
-		http.logout()
-			.logoutRequestMatcher(
-				new AntPathRequestMatcher("/logout"))    // 사용자 인증
-			.logoutSuccessUrl("/login")
-			.invalidateHttpSession(true);
-
-		// 권한이 없는 사용자가 접근했을 경우 이동할 경로
-		http.exceptionHandling()
-			.accessDeniedPage("/denied");
-	}
-
-	// 사용자 인증
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(memberService)
-			.passwordEncoder(getPasswordEncoder());
 	}
 }
