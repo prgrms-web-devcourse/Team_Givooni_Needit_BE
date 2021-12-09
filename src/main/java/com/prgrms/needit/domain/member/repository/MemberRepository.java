f0c969496f1815c1c053c72a77b1f880ccc1b80d
이하정 <961210eee@naver.com>
2021-12-10 05:36:49 +0900

    refactor : (CenterRepository, MemberRepository) findByIdAndIsDeletedFalse 추가

--- src/main/java/com/prgrms/needit/domain/member/repository/MemberRepository.java
+++ src/main/java/com/prgrms/needit/domain/member/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package com.prgrms.needit.domain.member.repository;
 
 import com.prgrms.needit.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+	Optional<Member> findByIdAndIsDeletedFalse(Long memberId);
 }
