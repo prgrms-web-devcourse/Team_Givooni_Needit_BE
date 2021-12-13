847a252b19690f6a95f829334c1fd69e837f5694
healtheefart <961210eee@naver.com>
2021-12-13 19:09:48 +0900

    refactor : response 필드에 final 제거

--- src/main/java/com/prgrms/needit/common/domain/dto/IsUniqueResponse.java
+++ src/main/java/com/prgrms/needit/common/domain/dto/IsUniqueResponse.java
@@ -2,7 +2,7 @@ package com.prgrms.needit.common.domain.dto;
 
 public class IsUniqueResponse {
 
-	private final boolean isUnique;
+	private boolean isUnique;
 
 	public IsUniqueResponse(boolean isUnique) {
 		this.isUnique = isUnique;
