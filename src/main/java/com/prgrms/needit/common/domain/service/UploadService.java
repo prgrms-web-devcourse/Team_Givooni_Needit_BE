8691065f0ed82863fc4f193bf3755fb1778c6158
김부희 <kimbuhui@naver.com>
2021-12-13 16:40:55 +0900

    NEED-87 feat : S3 설정
    - S3 관련 의존성 추가
    - S3 관련 환경변수 설정
    - UploadService 파일 업로드하는 코드 작성

---
+++ src/main/java/com/prgrms/needit/common/domain/service/UploadService.java
@@ -0,0 +1,39 @@
+package com.prgrms.needit.common.domain.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.util.IOUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class UploadService {
+
+	private static final String BUCKET = "needit-image";
+
+	private final AmazonS3Client amazonS3Client;
+
+	public String upload(MultipartFile file, String dirName) throws IOException {
+		InputStream uploadFile = file.getInputStream();
+		String fileName = dirName + "/" + uploadFile;
+
+		ObjectMetadata objectMetadata = new ObjectMetadata();
+		byte[] bytes = IOUtils.toByteArray(file.getInputStream());
+		objectMetadata.setContentLength(bytes.length);
+
+		amazonS3Client.putObject(
+			new PutObjectRequest(BUCKET, fileName, file.getInputStream(), objectMetadata)
+				.withCannedAcl(CannedAccessControlList.PublicRead)
+		);
+		System.out.println("3");
+		return amazonS3Client
+			.getUrl(BUCKET, fileName)
+			.toString();
+	}
+}
