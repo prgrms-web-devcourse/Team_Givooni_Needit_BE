ec5e0b659e28f34afc72709ed5e400784d88b8ee
healtheefart <961210eee@naver.com>
2021-12-13 16:49:31 +0900

    refactor : (develop update 후 오류나는 부분 수정) EmailRequest.email final 제거

--- src/main/java/com/prgrms/needit/domain/user/email/dto/EmailRequest.java
+++ src/main/java/com/prgrms/needit/domain/user/email/dto/EmailRequest.java
@@ -1,11 +1,13 @@
 package com.prgrms.needit.domain.user.email.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class EmailRequest {
 
-	private final String email;
+	private String email;
 
 	public EmailRequest(String email) {
 		this.email = email;
