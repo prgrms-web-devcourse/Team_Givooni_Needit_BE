5fa673e723e6da2c20fca8d9a8e5325552f22acc
healtheefart <961210eee@naver.com>
2021-12-16 19:06:40 +0900

    refactor : CenterListResponse -> CentersResponse로 수정

--- src/main/java/com/prgrms/needit/domain/user/center/dto/CentersResponse.java
+++ src/main/java/com/prgrms/needit/domain/user/center/dto/CentersResponse.java
@@ -4,13 +4,13 @@ import com.prgrms.needit.domain.user.center.entity.Center;
 import lombok.Getter;
 
 @Getter
-public class CenterListResponse {
+public class CentersResponse {
 
 	private Long centerId;
 	private String name;
 	private String profileImageUrl;
 
-	public CenterListResponse(Center center) {
+	public CentersResponse(Center center) {
 		this.centerId = center.getId();
 		this.name = center.getName();
 		this.profileImageUrl = center.getProfileImageUrl();
