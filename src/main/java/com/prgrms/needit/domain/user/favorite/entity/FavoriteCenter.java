be8aff7cfeef3b343cb414dd50d1cbcd32ceaba8
healtheefart <961210eee@naver.com>
2021-12-16 17:46:26 +0900

    refactor : FavoriteCenter BaseEntity 상속 받지 않게 수정

--- src/main/java/com/prgrms/needit/domain/user/favorite/entity/FavoriteCenter.java
+++ src/main/java/com/prgrms/needit/domain/user/favorite/entity/FavoriteCenter.java
@@ -1,23 +1,37 @@
 package com.prgrms.needit.domain.user.favorite.entity;
 
-import com.prgrms.needit.common.domain.entity.BaseEntity;
 import com.prgrms.needit.domain.user.center.entity.Center;
 import com.prgrms.needit.domain.user.member.entity.Member;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.util.Assert;
 
 @Getter
 @Entity
 @Table(name = "favorite_center")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class FavoriteCenter extends BaseEntity {
+public class FavoriteCenter {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
 
 	@ManyToOne
 	@JoinColumn(name = "member_id", referencedColumnName = "id")
