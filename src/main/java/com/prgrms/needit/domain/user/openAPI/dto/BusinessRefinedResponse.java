dcf1e1dc0c0bbd597bc66bb372fd53f5559c7414
healtheefart <961210eee@naver.com>
2021-12-19 05:32:50 +0900

    refactor : pr review 반영 - OpenApi.Dto 수정

--- src/main/java/com/prgrms/needit/domain/user/openAPI/dto/BusinessRefinedResponse.java
+++ src/main/java/com/prgrms/needit/domain/user/openAPI/dto/BusinessRefinedResponse.java
@@ -1,28 +1,25 @@
 package com.prgrms.needit.domain.user.openAPI.dto;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class BusinessRefinedResponse {
 
-	@JsonProperty("valid")
 	private boolean valid;
-
-	@JsonProperty("valid_msg")
-	private String valid_msg;
-
-	@JsonProperty("status_code")
+	private String validMsg;
 	private String status_code;
 
 	public BusinessRefinedResponse(BusinessRawResponse response) {
-		this.status_code = response.getStatus_code();
+		this.status_code = response.getStatusCode();
 		BusinessRawResponse.Data data = response.getData()
 												.get(0); // 한 건 조회
 		this.valid = data.getValid()
 						 .equals("01");
-		this.valid_msg = data.getValid_msg();
+
+		this.validMsg = data.getValidMsg();
 	}
 
 }
