9674da48c38a7c6f843c8744bb550037f7847fc5
healtheefart <961210eee@naver.com>
2021-12-19 06:04:53 +0900

    refactor : pr review 반영 - OpenApiController.checkBusinesscode 반환값 수정

--- src/main/java/com/prgrms/needit/domain/user/openAPI/controller/OpenApiController.java
+++ src/main/java/com/prgrms/needit/domain/user/openAPI/controller/OpenApiController.java
@@ -6,6 +6,7 @@ import com.prgrms.needit.domain.user.openAPI.dto.BusinessRequest;
 import com.prgrms.needit.domain.user.openAPI.service.OpenApiService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,11 @@ public class OpenApiController {
 	private final OpenApiService openAPIService;
 
 	@PostMapping("/check-businesscode")
-	public ApiResponse<BusinessRefinedResponse> checkBusinesscode(
+	public ResponseEntity<ApiResponse<BusinessRefinedResponse>> checkBusinesscode(
 		@Valid @RequestBody BusinessRequest request
 	) {
-		return ApiResponse.of(openAPIService.checkBusinesscode(request));
+		return ResponseEntity.ok(
+			ApiResponse.of(openAPIService.checkBusinesscode(request)));
 	}
 }
 
